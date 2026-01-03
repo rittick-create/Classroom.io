7407986bfb9b2961f4ec2e76f516fff18f4806a2
RITTICK DUTTA <rittickdutt369@gmail.com>
2026-01-03 22:45:55 +0530

    updated routes for userModel

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,47 +1,17 @@
-import  mongoose from "mongoose";
-const Schema = mongoose.Schema;
+
+const mongoose = require('mongoose');
 
 
-const userSchema = new Schema({
-    firstName: {
-        type: String,
-        uppercase: true,
-        required: true
-    },
-    lastName: {
-        type: String,
-        uppercase: true,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    confirmPassword: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-       //add validator later
-        unique: true,
-        required: true
-    },
-    phone: {
-        type: Number,
-        minlength: 10,
-        unique: true,
-        required: true
-    },
-    gender: {
-        type: String,
-        required: true
-    },
-    age: {
-        type: Number,
-        required: true
-    },    
-});
+const connectDB = async () => {
+  try {
+    
+    await mongoose.connect(process.env.MONGO_URI);
+    
+    console.log('MongoDB Connected Successfully');
+  } catch (error) {
+    console.error('MongoDB Connection Error:', error.message);
+    process.exit(1);
+  }
+};
 
-const User= mongoose.model("User",userSchema);
-export default User;
+module.exports = connectDB;
